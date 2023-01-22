704be703abd29454e03c7bef90bdc6dad6a87338
Michael J. Simons <michael@simons.ac>
2023-01-22 09:04:46 +0100

    Polish.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 module org.neo4j.sqltocypher {
     requires org.jooq;
     requires org.neo4j.cypherdsl.core;
